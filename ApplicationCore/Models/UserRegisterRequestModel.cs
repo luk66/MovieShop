552f6fb8376eeeeaf76e6fc2e4af3f65343f8066
luk66 <lukuangyi6@gmail.com>
2021-10-29 09:18:18 -0700

    Added validation attributes

--- ApplicationCore/Models/UserRegisterRequestModel.cs
+++ ApplicationCore/Models/UserRegisterRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,27 @@ namespace ApplicationCore.Models
 {
     public class UserRegisterRequestModel
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(64)]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "The password should be at least 8 characters and not exceeding 100", MinimumLength = 8)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$",
+            ErrorMessage = "Password should have minimun 8 characters and should include one upper, lower, number and a special char")]
         public string  Password { get; set; }
-
+        
+        [Required]
+        [StringLength(50)]
         public string  FirstName { get; set; }
-
+        
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+        
+        [DataType(DataType.Date)]
+        // minimum age and maximum age
 
         public DateTime DateOfBirth { get; set; }
 
